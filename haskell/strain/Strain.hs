b22d402f4db20f517980a3340a4000c80d417c5c
Greg Marshall <greg@marshallworks.com>
2014-11-08 13:06:24 -0500

    Haskell Strain iteration 2

--- haskell/strain/Strain.hs
+++ haskell/strain/Strain.hs
@@ -2,8 +2,12 @@ module Strain (keep, discard) where
 
 keep :: (a -> Bool) -> [a] -> [a]
 keep _ [] = []
-keep f xs = [ x | x <- xs, f x ]
+keep f (x:xs)
+    | f x       = x : keep f xs
+    | otherwise = keep f xs
 
 discard :: (a -> Bool) -> [a] -> [a]
 discard _ [] = []
-discard f xs = [ x | x <- xs, not $ f x ]
+discard f (x:xs)
+    | not $ f x = x : discard f xs
+    | otherwise = discard f xs
