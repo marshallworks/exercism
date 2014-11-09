da71c5721455b39c25fa836f0a88496c039a9591
Greg Marshall <greg@marshallworks.com>
2014-11-09 13:12:02 -0500

    haskell phone numbers v2

--- haskell/phone-number/Phone.hs
+++ haskell/phone-number/Phone.hs
@@ -3,15 +3,13 @@ module Phone (areaCode, number, prettyPrint) where
 import Data.Char (isDigit)
 
 number :: String -> String
-number str
-    | tenDigits                = justDigits
-    | elevenDigits && oneFirst = drop 1 justDigits
-    | otherwise                = invalidNumber
+number str =
+    case length justDigits of
+        10 -> justDigits
+        11  | head justDigits == '1' -> tail justDigits
+        _  -> invalidNumber
     where
         justDigits = filter isDigit str
-        tenDigits = length justDigits == 10
-        elevenDigits = length justDigits == 11
-        oneFirst = take 1 justDigits == "1"
         invalidNumber = "0000000000"
 
 areaCode :: String -> String
@@ -21,6 +19,5 @@ prettyPrint :: String -> String
 prettyPrint str = "("++ac++") "++three++"-"++four
     where
         num = number str
-        ac = take 3 num
-        three = take 3 $ drop 3 num
-        four = take 4 $ drop 6 num
+        (ac, rest) = splitAt 3 num
+        (three, four) = splitAt 3 rest
