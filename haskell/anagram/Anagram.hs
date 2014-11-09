831e79b5000fa2ccebd677b3f9c05a9dfcd6cf1a
Greg Marshall <greg@marshallworks.com>
2014-11-09 16:15:36 -0500

    haskell anagram v3

--- haskell/anagram/Anagram.hs
+++ haskell/anagram/Anagram.hs
@@ -4,21 +4,9 @@ import Data.Char (toLower)
 import Data.List (sort)
 
 anagramsFor :: String -> [String] -> [String]
-anagramsFor word possibilities = filter (isAnagram loweredWord sortedWord) possibilities
+anagramsFor word = filter isAnagram
     where
-    	loweredWord = lowerCaseWord word
-    	sortedWord = sortWordChars loweredWord
-
-isAnagram :: String -> String -> String -> Bool
-isAnagram loweredWord sortedWord match = notSameWord && anagram
-    where
-        loweredMatch = lowerCaseWord match
-        sortedMatch = sortWordChars loweredMatch
-        notSameWord = loweredWord /= loweredMatch
-        anagram = sortedWord == sortedMatch
-
-lowerCaseWord :: String -> String
-lowerCaseWord = map toLower
-
-sortWordChars :: String -> String
-sortWordChars = sort
+        lowerCaseStr = map toLower
+        loweredWord = lowerCaseStr word
+        sortedWord = sort loweredWord
+        isAnagram match = (loweredWord /= lowerCaseStr match) && (sortedWord == sort (lowerCaseStr match))
