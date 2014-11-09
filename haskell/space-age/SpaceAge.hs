880435e27dd1fb1c9d43a5a1f7bc17ade67a912c
Greg Marshall <greg@marshallworks.com>
2014-11-09 16:21:31 -0500

    haskell space age v3

--- haskell/space-age/SpaceAge.hs
+++ haskell/space-age/SpaceAge.hs
@@ -11,22 +11,13 @@ data Planet = Mercury
     deriving (Show)
 
 ageOn :: Planet -> Float -> Float
-ageOn planet secs =
+ageOn planet secs = secs / 31557600 /
     case planet of
-        Mercury -> secs/secsInYearMercury
-        Venus   -> secs/secsInYearVenus
-        Earth   -> secs/secsInYearEarth
-        Mars    -> secs/secsInYearMars
-        Jupiter -> secs/secsInYearJupiter
-        Saturn  -> secs/secsInYearSaturn
-        Uranus  -> secs/secsInYearUranus
-        Neptune -> secs/secsInYearNeptune
-        where
-            secsInYearEarth = 31557600
-            secsInYearMercury = secsInYearEarth * 0.2408467
-            secsInYearVenus = secsInYearEarth * 0.61519726
-            secsInYearMars = secsInYearEarth * 1.8808158
-            secsInYearJupiter = secsInYearEarth * 11.862615
-            secsInYearSaturn = secsInYearEarth * 29.447498
-            secsInYearUranus = secsInYearEarth * 84.016846
-            secsInYearNeptune = secsInYearEarth * 164.79132
+        Mercury -> 0.2408467
+        Venus   -> 0.61519726
+        Earth   -> 1
+        Mars    -> 1.8808158
+        Jupiter -> 11.862615
+        Saturn  -> 29.447498
+        Uranus  -> 84.016846
+        Neptune -> 164.79132
