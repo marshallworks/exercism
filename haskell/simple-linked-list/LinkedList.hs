ddbc83b57d2a192e09279822821bef633825af4c
Greg Marshall <greg@marshallworks.com>
2014-11-28 08:07:27 -0500

    haskell simple linked list simplified

--- haskell/simple-linked-list/LinkedList.hs
+++ haskell/simple-linked-list/LinkedList.hs
@@ -1,45 +1,29 @@
 module LinkedList where
 
-data Element a = Nil | Item a (Element a) deriving (Show)
-type LinkedList a = (Element a, Element a)
+data Element a = Nil | Element {
+    datum :: a,
+    next :: Element a
+} deriving (Show)
 
-nil :: LinkedList a
-nil = (Nil, Nil)
+nil :: Element a
+nil = Nil
 
-isNil :: LinkedList a -> Bool
-isNil (Nil, Nil) = True
-isNil (_, _)     = False
+isNil :: Element a -> Bool
+isNil Nil = True
+isNil _   = False
 
-new :: a -> LinkedList a -> LinkedList a
-new value (llhead, lltail) = ((Item value Nil), newTail llhead)
-    where
-        newTail Nil = Nil
-        newTail _   = Item (getValue llhead) lltail
+new :: a -> Element a -> Element a
+new = Element
 
-datum :: LinkedList a -> a
-datum (llhead, _) = getValue llhead
-
-next :: LinkedList a -> LinkedList a
-next (_, Nil)    = (Nil, Nil)
-next (_, lltail) = (Item (getValue lltail) Nil, (getTail lltail))
-
-fromList :: [a] -> LinkedList a
+fromList :: [a] -> Element a
 fromList = foldr new nil
 
-toList :: LinkedList a -> [a]
-toList lli = reverse $ accumList lli []
+toList :: Element a -> [a]
+toList ele = reverse $ gatherList [] ele
 
-reverseLinkedList :: LinkedList a -> LinkedList a
-reverseLinkedList lli = fromList $ accumList lli []
+reverseLinkedList :: Element a -> Element a
+reverseLinkedList ele = fromList $ gatherList [] ele
 
-accumList :: LinkedList a -> [a] -> [a]
-accumList (Nil, Nil) acc = acc
-accumList lli acc = accumList (next lli) (datum lli : acc)
-
-getValue :: Element a -> a
-getValue Nil        = error "should not happen"
-getValue (Item a _) = a
-
-getTail :: Element a -> Element a
-getTail Nil              = Nil
-getTail (Item _ theTail) = theTail
+gatherList :: [a] -> Element a -> [a]
+gatherList acc Nil = acc
+gatherList acc ele = gatherList (datum ele : acc) (next ele)
