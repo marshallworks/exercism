171d08c58ffede9104f7efe92c55e040ade168b5
Greg Marshall <greg@marshallworks.com>
2014-12-21 11:32:50 -0500

    haskell word count v2

--- haskell/word-count/WordCount.hs
+++ haskell/word-count/WordCount.hs
@@ -1,22 +1,16 @@
 module WordCount (wordCount) where
 
-import qualified Data.Map.Strict as DMS (Map, fromList, insertWith)
-import Data.Char (isAlphaNum, toLower)
+import qualified Data.Map.Strict as M (Map, fromList, insertWith)
+import Data.Char (toLower)
+import Data.List (foldl')
+import Data.List.Split (wordsBy)
 
 type Count = Int
 type Word = String
-type CountedWords = DMS.Map Word Count
+type CountedWords = M.Map Word Count
 
 wordCount :: String -> CountedWords
-wordCount str = gatherWordCount (DMS.fromList []) (wordList str)
-    where gatherWordCount = foldl (\ counts x -> DMS.insertWith (+) x 1 counts)
-
-wordList :: String -> [Word]
-wordList = gatherWordList [] ""
+wordCount str = gatherWordCount (M.fromList []) (wordList str)
     where
-        gatherWordList list "" [] = list
-        gatherWordList list word [] = word : list
-        gatherWordList list word (x:xs)
-            | isAlphaNum x    = gatherWordList list (word ++ [toLower x]) xs
-            | not (null word) = gatherWordList (word : list) "" xs
-            | otherwise       = gatherWordList list "" xs
+        wordList = wordsBy (`elem` " _-.,:!?@#$%^&*()[]{}") . map toLower
+        gatherWordCount = foldl' (\ counts x -> M.insertWith (+) x 1 counts)
